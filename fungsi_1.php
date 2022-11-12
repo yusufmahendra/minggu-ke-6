a29c0851b0aefc46c8f9a5af51b1805012e9ee5c
yusufmahendra <yusufm260901@gmail.com>
2022-11-12 06:40:11 -0800

    fungsi 1.php

---
+++ fungsi_1.php
@@ -0,0 +1,18 @@
+<?php
+function salam($waktu, $nama)
+{
+    return "Selamat $waktu, $nama!";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+    content="width=device-width, initial-scalc=1.0">
+    <title>Latihan Function</title>
+</head>
+<body>
+    <h1><?=salam("Pagi", "Eka") ?></h1>
+</body>
+</html>
