13978a6dbf5683f84232cb84734aafea9eb24fbf
yusufmahendra <yusufm260901@gmail.com>
2022-11-12 06:22:44 -0800

    TASK 6

--- date.php
+++ date.php
@@ -35,9 +35,12 @@
 
 ?>
 <br>
-
 <?php
 $d=mktime(04, 35, 30, 9, 26, 2001);
 echo "Tanggal Lahir Saya Adalah " . date("Y-m-d h:i:sa", $d);
 ?>
-
+<br>
+<?php
+$d=strtotime("10:30pm September 26 2030");
+echo "Tanggal Lahir Saya Adalah " . date("Y-m-d h:i:sa", $d);
+?>
