bb041b65bed611034c76a36e6a4a8b618e5c1230
yusufmahendra <yusufm260901@gmail.com>
2022-11-12 06:57:51 -0800

    TASK 8

---
+++ fungsi.php
@@ -0,0 +1,10 @@
+<?php
+function perkalian($angka1, $angka2)
+{
+    return "Perkalian $angka1, $angka2!";
+}
+$hasil = (60);
+echo "Perkalian 20 x 3 adalah $hasil";
+echo "<br>";
+echo "Perkalian 9 x 2 adalah " . (18);
+?>
